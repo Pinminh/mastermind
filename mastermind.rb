9f40c1b5fe2db3281986e9ecd125883b7b57ea23
Pinminh <minh.nguyenhongk22@hcmut.edu.vn>
2024-07-19 17:07:50 +0700

    test: run test for bot and player mode

--- mastermind.rb
+++ mastermind.rb
@@ -1,5 +1,8 @@
 require_relative 'lib/mastermind_game'
 
-game = MastermindGame.new
+num_slots = 4
+num_colors = 9
+num_turns = 6
+game = MastermindGame.new(num_slots, num_colors, num_turns)
 
 game.play_cli
